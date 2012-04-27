eee7d012e462d4a0810b6577400fb58019e349ba
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-26 20:35:01 -0400

    Started adding the exercises to flowchart & little edit on code

--- javascript-code.js
+++ javascript-code.js
@@ -26,15 +26,18 @@ var myLibrary = function () {
 		}
 		return false;		
 	};
-
+	var isURL = function (url) {
+		return url.indexOf("http:")==0 || url.indexOf("https:")==0;
+	};
 	return {
 		"isANumber": isANumber,
-		"isEmail": isEmail
+		"isEmail": isEmail,
+		"isURL": isURL
 	};
 };
 var exercise1 = function () {
 	var lib = myLibrary();
-	 stringChar = prompt("Input phone number");
+	var stringChar = prompt("Input phone number");
 	var inIsANumFunction = lib.isANumber(stringChar);
 	if (inIsANumFunction == true) {
 		console.log("Does a string follow a 123-456-7890 patter\nlike a phone number? The answer is " + inIsANumFunction + ".");
@@ -47,14 +50,24 @@ var exercise1 = function () {
 exercise1();
 var exercise2 = function() {
 	var lib = myLibrary();
-	 stringEmail = prompt("Input email address");
-	 var callIsEmail = lib.isEmail(stringEmail);
-	 if (callIsEmail == true) {
-		console.log("Does a string follow an aaa@bbb.ccc pattern like an email address? The answer is " + callIsEmail + ".");
-		
-	} else {
-		console.log("Does not follow an email pattern.");
-
-	}
+	var stringEmail = prompt("Input email address");
+	var callIsEmail = lib.isEmail(stringEmail);
+		if (callIsEmail == true) {
+			console.log("Does a string follow an aaa@bbb.ccc pattern like an email address? The answer is " + callIsEmail + ".");
+		} else {
+			console.log("Does not follow an email pattern.");
+		}
 };
 exercise2();
+var exercise3 = function () {
+	var lib = myLibrary();
+	var saveURL = prompt("Does it start with http: or https:?");
+	lib.isURL(saveURL);
+		console.log(lib.isURL(saveURL));
+		if (lib.isURL(saveURL) == true) {
+			console.log("This is correct of the URL");
+		} else {
+			console.log("Must pick between the two.");
+		}
+};
+exercise3();
