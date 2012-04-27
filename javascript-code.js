8133a8f6d74091384043315089b21ebe3029c6d9
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-26 22:52:47 -0400

    Fixed some mistakes and added function 6 exercise and library, also the added section to the FlowChart

--- javascript-code.js
+++ javascript-code.js
@@ -14,62 +14,65 @@ var myLibrary = function () {
 		return true;
 	};
 	var isEmail = function (email) {
-		b1 = false;
+		var b1 = false;
 		for (var e = 0; e < email.length; e++) {
 			if (email.charAt(e) == '@' && e != 0 && !b1) {
 				b1 = true;
 			
 			}
-			if (email.charAt(e) == '.' && b1 && e != email.length-1) {
+			if (email.charAt(e) == '.' && b1 && e != email.length -1) {
 				return true;
 			}
 		}
 		return false;		
 	};
 	var isURL = function (url) {
-		return url.indexOf("http:")==0 || url.indexOf("https:")==0;
+		return url.indexOf("http:") == 0 || url.indexOf("https:") == 0;
 	};
 	var titleCase = function (word) {
-		var a= word.split(" ");
-		var str= "";
-		for(var i=0; i<a.length; i++) {
-			str+= a[i].charAt(0).toUpperCase();
-			if(a.length>0) 
-				str+=a[i].substring(1);
+		var a = word.split(" ");
+		var str = "";
+		for (var i = 0; i < a.length; i++) {
+			str += a[i].charAt(0).toUpperCase();
+			if (a.length > 0) 
+				str += a[i].substring(1);
 				console.log(str);
-			if (i!=a.length) 
-				str+=" ";
+			if (i != a.length) 
+				str += " ";
 		}
 		return str;
 	};	
 	var separator = function (list,sep1,sep2) {
 		var old ="";
 		var n = list;
-			while(n != old) {
+			while (n != old) {
 		 		old = n;
 		 		n = n.replace(sep1,sep2);
 		 	}	
 		 	return n;
 	};
+	 var format = function (num,places) {
+    	var num = parseFloat(num);
+		return num.toFixed(places);
+	};	
 	return {
 		"isANumber": isANumber,
 		"isEmail": isEmail,
 		"isURL": isURL,
 		"titleCase": titleCase,
-		"separator": separator
+		"separator": separator,
+		"format": format
 	};
 };
 var exercise1 = function () {
-	lib = myLibrary();
+	var lib = myLibrary();
 	var stringChar = prompt("Input phone number");
 	var inIsANum = lib.isANumber(stringChar);
-	if (inIsANum == true) {
-		console.log("Does a string follow a 123-456-7890 patter\nlike a phone number? The answer is " + inIsANumFunction + ".");
-		
-	} else {
-		console.log("Does not follow a phone number pattern.");
-
-	}	
+		if (inIsANum == true) {
+			console.log("Does a string follow a 123-456-7890 patter\nlike a phone number? The answer is " + inIsANum + ".");
+		} else {
+			console.log("Does not follow a phone number pattern.");
+		}	
 };
 exercise1();
 var exercise2 = function() {
@@ -110,7 +113,14 @@ var exercise5 = function () {
 	console.log(callSeparator);
 };
 exercise5();
-
+var exercise6 = function () {
+	var lib = myLibrary();
+	var inputNum = prompt("Input number:");
+	var inputPlace = prompt("What decimal place:");
+	var callFormat = lib.format(inputNum,inputPlace);
+	console.log(callFormat);
+};
+exercise6();
 
 
 
