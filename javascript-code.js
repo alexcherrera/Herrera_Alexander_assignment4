4b696194e22d6adeef53a0f7d5f11cc966e073c9
Alexander Herrera <alexcherrera@fullsail.edu>
2012-04-26 20:01:29 -0400

    Changed around my codes, made exercise 1 & 2 simpler and took out exercise 3 & 9

--- javascript-code.js
+++ javascript-code.js
@@ -38,29 +38,6 @@ var exercise1 = function () {
 	var inIsANumFunction = lib.isANumber(stringChar);
 	if (inIsANumFunction == true) {
 		console.log("Does a string follow a 123-456-7890 patter\nlike a phone number? The answer is " + inIsANumFunction + ".");
-		lengthSection: {
-			var	calLength = stringChar.length;
-			console.log("The total length is: " + calLength + ".");
-		}
-		allDashSigns: {
-			var firstDash = stringChar.indexOf("-");
-			var finalDash = stringChar.lastIndexOf("-");
-			console.log("The first dash is on index: " + firstDash + ".");
-		}
-		firstSection: {
-			var zipCode = stringChar.substring(0,firstDash);
-			console.log("The first set of numbers (zipCode): " + zipCode + ".");
-		}
-		SecondSection: {
-			var afterfirstDashPos = firstDash + 1;
-			var secondDigits = stringChar.substring(afterfirstDashPos,finalDash-1);
-			console.log("The second set of numbers: " + secondDigits + ".");
-			console.log("The last dash is on the index: " + finalDash + ".");
-		}
-		ThirdSection: {
-			var storeSetNum = stringChar.substring(finalDash + 1 ,calLength - 1);
-			console.log("The last set of numbers: " + storeSetNum + ".\n");
-		}
 		
 	} else {
 		console.log("Does not follow a phone number pattern.");
